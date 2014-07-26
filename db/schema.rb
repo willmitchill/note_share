62793d18e0f85822023b342a3d9b0acae2e56f82
Will <wmitchel@hotmail.com>
2014-07-26 14:15:09 -0700

    added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321144528) do
+ActiveRecord::Schema.define(version: 20140726125050) do
 
   create_table "users", force: true do |t|
-    t.string "name"
-    t.string "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
+    t.string   "integer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
